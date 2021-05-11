f41a92ba4ad0a2da312d44563b9f345ecc8ea4fe
Valentin Vago <65971+zeropaper@users.noreply.github.com>
2021-05-11 13:59:38 +0000

    [core] add react-hook-form stories

--- src/mantine-core/src/TextInput/TextInput.story.tsx
+++ src/mantine-core/src/TextInput/TextInput.story.tsx
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
 import { DEFAULT_THEME, MantineProvider } from '@mantine/theme';
 import { LockClosedIcon } from '@modulz/radix-icons';
+import { Controller, FieldError, useForm } from 'react-hook-form';
 import { TextInput } from './TextInput';
+import { Button } from '../Button/Button';
 
 function WrappedTextInput(
   props: Omit<React.ComponentProps<typeof TextInput>, 'value' | 'onChange'>
@@ -87,4 +89,147 @@ storiesOf('@mantine/core/TextInput', module)
         />
       </div>
     </MantineProvider>
-  ));
+  ))
+  // unfortunately, the usage of "form.register" is not working (with defaultValues)
+  // but using the Controller component (below) will do the trick (although more verbose)
+  .add('react-hook-form register', () => {
+    const form = useForm<{
+      optionalString?: string;
+      requiredString: string;
+    }>({
+      defaultValues: {
+        optionalString: 'not needed',
+      },
+    });
+    const [lastSubmission, setLastSubmission] = useState<{ [k: string]: string } | null>(null);
+
+    const { errors } = form.formState;
+    const getErrorMessage = (fieldError?: FieldError) => {
+      if (!fieldError) return null;
+
+      if (fieldError.message) return fieldError.message;
+
+      switch (fieldError.type) {
+        case 'required':
+          return 'This field is required';
+        default:
+          return 'The value of this field is invalid';
+      }
+    };
+
+    return (
+      <div style={{ display: 'flex', padding: '4px' }}>
+        <form onSubmit={form.handleSubmit((submitted) => setLastSubmission(submitted))}>
+          <TextInput
+            label="Optional String"
+            {...form.register('optionalString')}
+            error={getErrorMessage(errors?.optionalString)}
+          />
+
+          <TextInput
+            label="Required String"
+            required
+            {...form.register('requiredString', { required: true })}
+            error={getErrorMessage(errors?.requiredString)}
+          />
+
+          <Button type="submit">Submit</Button>
+        </form>
+
+        <div>
+          <div><code>form.getValues()</code></div>
+          <pre>{JSON.stringify(form.getValues(), null, 2)}</pre>
+
+          <hr />
+
+          <div>
+            <div><code>form.watch(&apos;optionalString&apos;)</code></div>
+            <div>{form.watch('optionalString')}</div>
+          </div>
+
+          <hr />
+
+          <div>last submitted values</div>
+          <pre>{JSON.stringify(lastSubmission, null, 2)}</pre>
+        </div>
+      </div>
+    );
+  })
+  .add('react-hook-form controller', () => {
+    const form = useForm<{
+      optionalString?: string;
+      requiredString: string;
+    }>({
+      defaultValues: {
+        optionalString: 'not needed',
+      },
+    });
+    const [lastSubmission, setLastSubmission] = useState<{ [k: string]: string } | null>(null);
+
+    const getErrorMessage = (fieldError?: FieldError) => {
+      if (!fieldError) return null;
+
+      if (fieldError.message) return fieldError.message;
+
+      switch (fieldError.type) {
+        case 'required':
+          return 'This field is required';
+        default:
+          return 'The value of this field is invalid';
+      }
+    };
+
+    return (
+      <div style={{ display: 'flex', padding: '4px' }}>
+        <form onSubmit={form.handleSubmit((submitted) => setLastSubmission(submitted))}>
+          <Controller
+            name="optionalString"
+            control={form.control}
+            render={({ field: { ref, onBlur, onChange } }) => (
+              <TextInput
+                label="Optional String"
+                elementRef={ref}
+                onBlur={onBlur}
+                onChange={onChange}
+              />
+            )}
+          />
+
+          <Controller
+            name="requiredString"
+            control={form.control}
+            rules={{ required: true }}
+            render={({ field: { ref, onBlur, onChange }, fieldState: { error } }) => (
+              <TextInput
+                label="Required String"
+                required
+                elementRef={ref}
+                onBlur={onBlur}
+                onChange={onChange}
+                error={getErrorMessage(error)}
+              />
+            )}
+          />
+
+          <Button type="submit">Submit</Button>
+        </form>
+
+        <div>
+          <div><code>form.getValues()</code></div>
+          <pre>{JSON.stringify(form.getValues(), null, 2)}</pre>
+
+          <hr />
+
+          <div>
+            <div><code>form.watch(&apos;optionalString&apos;)</code></div>
+            <div>{form.watch('optionalString')}</div>
+          </div>
+
+          <hr />
+
+          <div>last submitted values</div>
+          <pre>{JSON.stringify(lastSubmission, null, 2)}</pre>
+        </div>
+      </div>
+    );
+  });
